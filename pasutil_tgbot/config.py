52095d7d1d744d9805dc77d3797f33998416af84
Walther <149716887+Dungeon01101@users.noreply.github.com>
2025-11-12 21:20:11 +0300

    Update config.py

--- pasutil_tgbot/config.py
+++ pasutil_tgbot/config.py
@@ -1,7 +1,8 @@
-TOKEN = "8361026443:AAEryy_H9iFzc8a4SmnCxP_fQLmZn0U-N5M"
-CHANNEL = "@pasutilchannel"
-API_KEY = "69a2ae462ad477d65eb8bb6d831b1d773eba9a8103ed567f770e5588a32ae457" 
+TOKEN = ""
+CHANNEL = "@"
+API_KEY = "" 
 TONCENTER_URL = f"https://toncenter.com/api/v2/jsonRPC?api_key={API_KEY}" 
-WATCH_ADDRESS_B64 = "UQBAdex3PPycBFQ33kMLy6D5FWwPiPQHYOe1295CVw8DLJFe"
+WATCH_ADDRESS_B64 = ""
+
 
 headers = {'Content-Type': 'application/json'}
