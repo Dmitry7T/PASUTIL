068ea9bd39abcce2998f0dbb2d6aa1c490b69d4a
Walther <149716887+Dungeon01101@users.noreply.github.com>
2025-11-12 21:19:37 +0300

    Add files via upload

--- pasutil_tgbot/config.py
+++ pasutil_tgbot/config.py
@@ -1,2 +1,7 @@
-TOKEN = ""
-CHANNEL = ""
+TOKEN = "8361026443:AAEryy_H9iFzc8a4SmnCxP_fQLmZn0U-N5M"
+CHANNEL = "@pasutilchannel"
+API_KEY = "69a2ae462ad477d65eb8bb6d831b1d773eba9a8103ed567f770e5588a32ae457" 
+TONCENTER_URL = f"https://toncenter.com/api/v2/jsonRPC?api_key={API_KEY}" 
+WATCH_ADDRESS_B64 = "UQBAdex3PPycBFQ33kMLy6D5FWwPiPQHYOe1295CVw8DLJFe"
+
+headers = {'Content-Type': 'application/json'}
