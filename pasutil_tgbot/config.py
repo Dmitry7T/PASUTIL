9496c9fc9d49cabc551533a424960e04fe8ea625
Walther <149716887+Dungeon01101@users.noreply.github.com>
2025-12-20 20:26:04 +0300

    послание

--- pasutil_tgbot/config.py
+++ pasutil_tgbot/config.py
@@ -1,3 +1,5 @@
 TOKEN = "8361026443:AAEryy_H9iFzc8a4SmnCxP_fQLmZn0U-N5M" #8361026443:AAEryy_H9iFzc8a4SmnCxP_fQLmZn0U-N5M  ТЕСТ = 7347793114:AAHSwjUWs3MUMuw9W7e2q8_SpPFiSIiDwmE
 CHANNEL = "@pasutilchannel"
 API_TOKEN = "489778:AAayLjv8eJq6uiWY2HK0XeO45d9Vnucf56F"
+
+#ТОТ, КТО СПИЗДИЛ ТОКЕН, ЗАПОМНИ - ТВОЯ МАТЬ СОСАЛА НАМ ССЕМ ЧЛЕН, ТВОЮ СОБАКУ МЫ ЗАЖАРИЛИ И СЪЕЛИ НА КОСТРЕ, А ТВОЕМУ ОТЦУ МЫ ЗАСУНУЛИ В ОЧКО БУТЫЛКУ ИЗ ПОД БУТЫЛКИ, И ВЫСУНУЛИ ЧЕРЕЗ РОТ. А ВСЯ ТВОЯ РОДОСЛОВНАЯ - КОРЕННЫЕ ПРОСТИТУТКИ
