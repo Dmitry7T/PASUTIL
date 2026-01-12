6349a16fd7fb22d6b686ad411c68afeea81a986d
Walther <149716887+Dungeon01101@users.noreply.github.com>
2026-01-12 21:22:41 +0300

    Delete pasutil_tgbot directory

--- pasutil_tgbot/config.py
+++
@@ -1,5 +0,0 @@
-TOKEN = "" #8361026443:AAHdAMqBIL0H283QYwSCNFRMvBVjGYyBATM  ТЕСТ = 7347793114:AAFYfsiZpy4iu1VtPbTIuPbQQELNvo19VlE
-CHANNEL = "@pasutilchannel"
-API_TOKEN = "489778:AAsAV1KVQLB4Wqy5wKl0Xkw33rT6bR3D48G"
-
-#ТОТ, КТО СПИЗДИЛ ТОКЕН, ЗАПОМНИ - ТВОЯ МАТЬ СОСАЛА НАМ ВСЕМ ЧЛЕН, ТВОЮ СОБАКУ МЫ ЗАЖАРИЛИ И СЪЕЛИ НА КОСТРЕ, А ТВОЕМУ ОТЦУ МЫ ЗАСУНУЛИ В ОЧКО БУТЫЛКУ ИЗ ПОД ВОДКИ, И ВЫСУНУЛИ ЧЕРЕЗ РОТ. А ВСЯ ТВОЯ РОДОСЛОВНАЯ - КОРЕННЫЕ ПРОСТИТУТКИ
